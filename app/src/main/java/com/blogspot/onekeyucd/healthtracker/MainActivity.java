569d632adf6948d6d9d32c69326eb153d85d38ed
Joe Lewis <joelewis@ucdavis.edu>
2016-04-06 13:04:06 -0700

    Added Game Reset Button (Visual Only)
    added the game reset button to the layout of the main activity

--- app/src/main/java/com/blogspot/onekeyucd/healthtracker/MainActivity.java
+++ app/src/main/java/com/blogspot/onekeyucd/healthtracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.blogspot.onekeyucd.healthtracker;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
 		if(player1.getView() != null) player1.getView().setBackgroundColor(getResources().getColor(R.color.IndianRed));
 		PlayerFragment player2 = (PlayerFragment)fragmentManager.findFragmentById(R.id.player2_fragment);
 		if(player1.getView() != null) player2.getView().setBackgroundColor(getResources().getColor(R.color.SkyBlue));
+	}
+
+	public void resetGame(View view) {
+		
 	}
 }
